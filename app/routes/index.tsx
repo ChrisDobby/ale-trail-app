7e77b0f8e1eb3f2c62bab58f072173ed30eae627
Chris Dobson <chrisdobby.dev@gmail.com>
2021-07-04 20:10:09 +0100

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,38 @@
+import type { MetaFunction, LinksFunction, LoaderFunction } from "remix";
+import { useRouteData } from "remix";
+
+import stylesUrl from "../styles/index.css";
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "Remix Starter",
+    description: "Welcome to remix!",
+  };
+};
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+export let loader: LoaderFunction = async () => {
+  return { message: "this is awesome 😎" };
+};
+
+export default function Index() {
+  let data = useRouteData();
+
+  return (
+    <div style={{ textAlign: "center", padding: 20 }}>
+      <h2>Welcome to Remix!</h2>
+      <p>
+        <a href="/two">Here's another page</a>
+      </p>
+      <p>Message from the loader: {data.message}</p>
+      <p>
+        <a href="https://github.com/ascorbic/remix-on-netlify/">
+          remix-on-netlify repo
+        </a>
+      </p>
+    </div>
+  );
+}
---
+++ app/routes/two.tsx
@@ -0,0 +1,10 @@
+export default function Two() {
+  return (
+    <div>
+      <h2>It's another page!</h2>
+      <p>
+        <a href="/">I've had enough</a>
+      </p>
+    </div>
+  );
+}
