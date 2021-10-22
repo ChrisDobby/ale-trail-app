77dc40f038ec8bb4b696ae23b96b36f7e6f1bf9a
chrisdobby <dobby@chrisjdobson.co.uk>
2021-10-22 17:51:13 +0100

    add viewport tag

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ function Document({ children, title }: { children: React.ReactNode; title?: stri
         <html lang="en">
             <head>
                 <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.png" type="image/png" />
                 {title ? <title>{title}</title> : null}
                 <Meta />
