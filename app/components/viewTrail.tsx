d4b5f012c710af3f7a35bac1de4dbf14783230f8
chrisdobby <dobby@chrisjdobson.co.uk>
2021-10-29 17:55:51 +0100

    add view trail component

---
+++ app/components/viewTrail.tsx
@@ -0,0 +1,57 @@
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import { useTheme } from "@mui/material/styles";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import format from "date-fns/format";
+import QrCode from "qrcode.react";
+import { Trail } from "../types";
+import StopList from "./stopList";
+
+type InviteProps = { id: string };
+function Invite({ id }: InviteProps) {
+    const joinUrl = `https://ale-trail.chrisdobby.dev/trail/join/${id}`;
+    return (
+        <Stack alignItems="center">
+            <Typography sx={{ padding: "0.5rem", textAlign: "center" }} variant="body1">
+                To join the trail use this link
+            </Typography>
+            <a href={joinUrl}>{joinUrl}</a>
+            <Typography sx={{ padding: "0.5rem", textAlign: "center" }} variant="body1">
+                or scan the code
+            </Typography>
+            <QrCode value={joinUrl} />
+        </Stack>
+    );
+}
+
+type ViewTrailProps = { trail: Trail; canStart: boolean; onStart: () => void };
+export default function ViewTrail({ trail, canStart, onStart }: ViewTrailProps) {
+    const theme = useTheme();
+
+    return (
+        <Stack sx={{ flexGrow: 1, bgcolor: theme.palette.action.disabledBackground }} spacing={2}>
+            <Typography sx={{ bgcolor: "background.paper", padding: "0.5rem", textAlign: "center" }} variant="h6">
+                {format(new Date(trail.meeting.dateTime), "dd-MMM-yyyy HH:mm")}
+            </Typography>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Details</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <StopList meeting={trail.meeting} stops={trail.stops} readOnly />
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>Invite</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Invite id={trail.id} />
+                </AccordionDetails>
+            </Accordion>
+        </Stack>
+    );
+}
--- app/routes/trail/$id.tsx
+++ app/routes/trail/$id.tsx
@@ -1,17 +1,20 @@
 import { useLoaderData } from "remix";
 import type { MetaFunction } from "remix";
 import format from "date-fns/format";
-import { secure } from "../../authentication";
+import { secure, AuthenticatedLoaderArgs, getAuthHeader, getUser } from "../../authentication";
 import { tokenCookie } from "../../cookies";
 import { getSession, commitSession } from "../../session";
 import { StoreLoaderArgs } from "../../store";
 import withStore from "../../withStore";
+import ViewTrail from "../../components/viewTrail";
 
 export const meta: MetaFunction = (params: any) => {
-    const { data } = params;
+    const {
+        data: { trail },
+    } = params;
     return {
-        title: `Ale trail - ${data?.meeting?.station?.name} ${format(
-            new Date(data?.meeting?.dateTime),
+        title: `Ale trail - ${trail?.meeting?.station?.name} ${format(
+            new Date(trail?.meeting?.dateTime),
             "dd-MMM-yyyy HH:mm",
         )}`,
         description: "",
@@ -20,17 +23,31 @@ export const meta: MetaFunction = (params: any) => {
 
 async function viewLoader({
     context: {
+        auth,
+        headers,
         store: { getTrail },
     },
     params,
-}: StoreLoaderArgs) {
-    return getTrail(params.id);
+}: AuthenticatedLoaderArgs & StoreLoaderArgs) {
+    const userResponse = await getUser({ ...headers, ...getAuthHeader(auth) });
+    const user = await userResponse.json();
+    const storedTrail = await getTrail(params.id);
+
+    const trail = {
+        ...storedTrail,
+        stops: Object.values(storedTrail.stops),
+    };
+
+    return { trail, canStart: trail.createdBy === user.sub };
 }
 
 export const loader = (args: any) =>
     secure({ cookie: tokenCookie, getSession, commitSession, args }, withStore(viewLoader));
 
-export default function ViewTrail() {
-    const trail = useLoaderData();
-    return <div>{JSON.stringify(trail)}</div>;
+export default function View() {
+    const { trail } = useLoaderData();
+
+    const handleStart = () => {};
+
+    return <ViewTrail trail={trail} canStart onStart={handleStart} />;
 }
